7b73dcafcb6ec25d4cc45963ead9fad83d151294
tlabna <tlabna@users.noreply.github.com>
2018-07-11 13:06:42 -0400

    feat: Add route /reverse/:name
    - Testing routes with express
    - chore: (lint + prettify)
      - routes/index.js
      - app.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-// Do work here
+/**
+  GET method for '/' that takes in a cb
+  req = object of data coming in
+  res = object full of methods of sending data back to user
+ */
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
+  // const t = { name: 'T', age: 25, cool: true }
+  // res.send('Hey! It works!')
+  // res.json(t)
+  res.send(req.query) // req.query allows us to get query params from URL
+})
 
-module.exports = router;
+router.get('/reverse/:name', (req, res) => {
+  // :name = URL parameter called name
+  const reverse = [...req.params.name].reverse().join('')
+  res.send(reverse)
+})
+
+module.exports = router
