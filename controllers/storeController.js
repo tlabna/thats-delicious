8ebc3212fe546c45e2e30834e6b07e4e23477639
tlabna <tlabna@users.noreply.github.com>
2018-07-11 20:16:00 -0400

    feat: Add display all stores and update routes
    - Add getStores func to storeController
      - We query for all stores and render to stores.pug template
    - Updates routes/index.js
      - GET: for '/' that displays all stores
      - GET: for '/stores' that displays all stores
    - Add stores.pug template
    - Add mixin _storeCard.pug for resuability
      - creates a card for each store passed to it
    - chore: Add some eslint rules I like for good practice

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -21,3 +21,10 @@ exports.createStore = async (req, res) => {
   )
   res.redirect(`/store/${store.slug}`)
 }
+
+exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find()
+
+  res.render('stores', { title: 'Stores', stores })
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const storeController = require('../controllers/storeController')
+const {
+  addStore,
+  createStore,
+  getStores,
+} = require('../controllers/storeController')
 const { catchErrors } = require('../handlers/errorHandlers')
 
-router.get('/', storeController.homePage)
-router.get('/add', storeController.addStore)
-router.post('/add', catchErrors(storeController.createStore))
+router.get('/', catchErrors(getStores))
+router.get('/stores', catchErrors(getStores))
+router.get('/add', addStore)
+router.post('/add', catchErrors(createStore))
 
 module.exports = router
