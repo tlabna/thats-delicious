78602534efe5c75a3869ee45a0909f4c25a942ce
tlabna <tlabna@users.noreply.github.com>
2018-07-12 11:03:10 -0400

    feat: Add geocoding data with Google Maps
    - Form will no suggest address when user types in an address
      - lat and lng will be calculated and inserted into correct fields
       on address selection
    - Add autoComplete.js
      - Will display dropdown when user types in address input
      - It will calculate lat+lng and insert input correct input fields
      - Add eventListener for hitting ENTER so form does not submit as well
    - Update delicious-app.js to run autoComplete()
    - chore: run eslint + prettier on bling.js, autoComplete.js
    and delicious-app.js

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,3 +1,6 @@
-import '../sass/style.scss';
+import '../sass/style.scss'
 
-import { $, $$ } from './modules/bling';
+import { $, $$ } from './modules/bling' // eslint-disable-line
+import autoComplete from './modules/autoComplete'
+
+autoComplete($('#address'), $('#lat'), $('#lng'))
