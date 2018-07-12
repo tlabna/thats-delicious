b4ec6151d60d1a58b8916414cd9e5f3b8810e5d7
tlabna <tlabna@users.noreply.github.com>
2018-07-11 21:29:03 -0400

    feat: Add ability to edit Store data and and apply to database
    - Add new routes
      - GET: '/stores/:id/edit'
      - POST: 'add/:id'
    - Add controllers:
      - editStore -> will render form with current store data
      - updateStore -> after submitting changes form will POST to '/add/:id'.
      Query that store and update db and redirect back to edit form with
      new changes.
    - Update editStore.pug by passing store to _storeForm mixin
    - Update _storeCard.pug with pencil button to link to edit form
    - Update _storeForm.pug to display store data
      - Note this is the same template for adding a store.
      If no store data exists then fields will be blank and form will
      POST to /add.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -28,3 +28,30 @@ exports.getStores = async (req, res) => {
 
   res.render('stores', { title: 'Stores', stores })
 }
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given an ID
+  const store = await Store.findOne({ _id: req.params.id })
+  // 2. Confirm they are the owner of the store
+  // TODO
+  // 3. Render out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store })
+}
+
+exports.updateStore = async (req, res) => {
+  // find and update the store
+  // findOneAndUpdate(q, data, options)
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true, // force model to run validators
+  }).exec() // exec() make sure query to run
+
+  req.flash(
+    'success',
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${
+      store.slug
+    }">View Store</a>`
+  )
+  // redirect to store and flash that update worked
+  res.redirect(`/stores/${store._id}/edit`)
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router()
 const {
   addStore,
   createStore,
+  editStore,
   getStores,
+  updateStore,
 } = require('../controllers/storeController')
 const { catchErrors } = require('../handlers/errorHandlers')
 
@@ -11,5 +13,7 @@ router.get('/', catchErrors(getStores))
 router.get('/stores', catchErrors(getStores))
 router.get('/add', addStore)
 router.post('/add', catchErrors(createStore))
+router.post('/add/:id', catchErrors(updateStore))
+router.get('/stores/:id/edit', catchErrors(editStore))
 
 module.exports = router
