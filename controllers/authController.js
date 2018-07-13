483b44427c336998bb0b7d8ba98d9bca9a9a0f93
tlabna <tlabna@users.noreply.github.com>
2018-07-13 17:29:44 -0400

    feat: Add logout functionality, add gravatar virtual field, protect /add
    - Add virtual field 'gravatar' to userSchema
    - Update authController.js
      - Add logout controller -> logs user out using passport .logout() func
      - Add isLoggedIn() middleware to check if user is authenticated
    - Add isLoggedIn() to /add (GET) route. Only logged in users can access
      this route. If not, then redirect to homepage and flash message
    - refactor: routes/index.js for better readability and finding routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,22 @@ exports.login = passport.authenticate('local', {
   successRedirect: '/',
   successFlash: 'You are now logged in.',
 })
+
+exports.logout = (req, res) => {
+  // passport.js logout() func
+  req.logout()
+  req.flash('success', 'You are now logged out.')
+  res.redirect('/')
+}
+
+exports.isLoggedIn = (req, res, next) => {
+  // 1. check if the user is authenticated
+  // passport.js isAuthenticated() func
+  if (req.isAuthenticated()) {
+    next() // carry on since they are logged in
+    return
+  }
+
+  req.flash('error', 'You must be logged in to do that.')
+  res.redirect('/')
+}
--- routes/index.js
+++ routes/index.js
@@ -17,21 +17,33 @@ const {
   validateRegister,
   register,
 } = require('../controllers/userController')
-const { login } = require('../controllers/authController')
+const { login, logout, isLoggedIn } = require('../controllers/authController')
 const { catchErrors } = require('../handlers/errorHandlers')
 
+/** index route */
 router.get('/', catchErrors(getStores))
+
+/** /stores and /store */
 router.get('/stores', catchErrors(getStores))
-router.get('/add', addStore)
-router.post('/add', upload, catchErrors(resize), catchErrors(createStore))
-router.post('/add/:id', upload, catchErrors(resize), catchErrors(updateStore))
 router.get('/stores/:id/edit', catchErrors(editStore))
 router.get('/store/:slug', catchErrors(getStoreBySlug))
+
+/** /add */
+router.get('/add', isLoggedIn, addStore)
+router.post('/add', upload, catchErrors(resize), catchErrors(createStore))
+router.post('/add/:id', upload, catchErrors(resize), catchErrors(updateStore))
+
+/** /tags */
 router.get('/tags', catchErrors(getStoresByTag))
 router.get('/tags/:tag', catchErrors(getStoresByTag))
-router.get('/login', loginForm)
-router.get('/register', registerForm)
 
+/** /login and /logout */
+router.get('/login', loginForm)
+router.post('/login', login)
+router.get('/logout', logout)
+
+/** /register */
+router.get('/register', registerForm)
 // 1. Validate the registration data
 // 2. Register the user
 // 3. Login user
